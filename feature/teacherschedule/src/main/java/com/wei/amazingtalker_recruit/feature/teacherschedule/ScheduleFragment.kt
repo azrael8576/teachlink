d0ef3e6e655cbb92ead9bc516d9b15bac9743071
azrael8576 <azrael8576love2008@gmail.com>
2023-05-10 00:50:09 +0800

    - Fix: 修正 教師行事曆 頁面，點擊上下星期按鈕，重複刷新 list 之問題

--- feature/teacherschedule/src/main/java/com/wei/amazingtalker_recruit/feature/teacherschedule/ScheduleFragment.kt
+++ feature/teacherschedule/src/main/java/com/wei/amazingtalker_recruit/feature/teacherschedule/ScheduleFragment.kt
@@ -6,30 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.doOnLayout
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.wei.amazingtalker_recruit.core.domain.GetTeacherScheduleTimeUseCase
-import com.wei.amazingtalker_recruit.core.extensions.getLocalOffsetDateTime
 import com.wei.amazingtalker_recruit.core.model.data.IntervalScheduleTimeSlot
-import com.wei.amazingtalker_recruit.core.model.data.ScheduleState
 import com.wei.amazingtalker_recruit.core.result.DataSourceResult
 import com.wei.amazingtalker_recruit.feature.teacherschedule.adapters.OnItemClickListener
 import com.wei.amazingtalker_recruit.feature.teacherschedule.adapters.ScheduleTimeListAdapter
 import com.wei.amazingtalker_recruit.feature.teacherschedule.databinding.FragmentScheduleBinding
-import com.wei.amazingtalker_recruit.feature.teacherschedule.utilities.TEACHER_SCHEDULE_TIME_INTERVAL
 import com.wei.amazingtalker_recruit.feature.teacherschedule.viewmodels.ScheduleViewModel
 import com.wei.amazingtalker_recruit.feature.teacherschedule.viewmodels.WeekAction
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
-import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
-import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
@@ -38,11 +31,9 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
 
     @Inject
     lateinit var adapter: ScheduleTimeListAdapter
-    @Inject
-    lateinit var getTeacherScheduleTimeUseCase: GetTeacherScheduleTimeUseCase
     private val viewModel: ScheduleViewModel by viewModels()
     private var binding: FragmentScheduleBinding? = null
-    private var mCurrentTabTag = ""
+    private var isUpdateWeek = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -84,8 +75,7 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 Timber.d("======onTabSelected====${tab.tag}")
 
-                mCurrentTabTag = tab.tag.toString()
-                updateAdapterList(adapter, viewModel.teacherScheduleTimeList.value)
+                viewModel.onTabSelected(tab.tag.toString())
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
@@ -127,55 +117,41 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
         }
 
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            viewModel.currentSearchResult.collect { result ->
-                when (result) {
-                    is DataSourceResult.Success -> {
-                        val scheduleTimeList = mutableListOf<IntervalScheduleTimeSlot>()
+            viewModel.filteredTimeList.collect { result ->
+                if (!isUpdateWeek) {
+                    when (result) {
+                        is DataSourceResult.Success -> {
+                            if (!isUpdateWeek) {
+                                result.data.let {
+                                    adapter.addHeaderAndSubmitList(
+                                        it
+                                    )
+                                }
+                                binding?.scheduleTimeRecyclerview?.scrollToPosition(0)
+                                Timber.d("API Success")
+                            }
+                        }
 
-                        scheduleTimeList.addAll(
-                            getTeacherScheduleTimeUseCase(
-                                result.data.available,
-                                TEACHER_SCHEDULE_TIME_INTERVAL,
-                                ScheduleState.AVAILABLE
-                            )
-                        )
-                        scheduleTimeList.addAll(
-                            getTeacherScheduleTimeUseCase(
-                                result.data.booked,
-                                TEACHER_SCHEDULE_TIME_INTERVAL,
-                                ScheduleState.BOOKED
-                            )
-                        )
+                        is DataSourceResult.Error -> {
+                            Toast.makeText(
+                                requireContext(),
+                                "Api Failed ${result.exception}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            Timber.d("API Failed ${result.exception}")
+                        }
 
-                        viewModel.setTeacherScheduleTimeList(scheduleTimeList)
-                        binding?.scheduleTimeRecyclerview?.isVisible = true
-
-                        Timber.d("API Success")
-                    }
-
-                    is DataSourceResult.Error -> {
-                        Toast.makeText(requireContext(), "Api Failed", Toast.LENGTH_SHORT).show()
-
-                        Timber.d("API Failed DataSourceResult.Error")
-                    }
-
-                    is DataSourceResult.Loading -> {
-
-                        Timber.d("API Loading")
-                    }
-
-                    else -> {
-
-                        Timber.e("API Else")
+                        is DataSourceResult.Loading -> {
+                            Timber.d("API Loading")
+                        }
                     }
                 }
-            }
-        }
 
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            viewModel.teacherScheduleTimeList.collect {
-                updateAdapterList(adapter, it)
+                if (isUpdateWeek) {
+                    isUpdateWeek = false
+                }
             }
+
         }
 
     }
@@ -187,7 +163,7 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
         } else {
             binding.buttonLastWeek.setColorFilter(resources.getColor(R.color.amazingtalker_green_900))
             binding.buttonLastWeek.setOnClickListener(View.OnClickListener {
-                binding.scheduleTimeRecyclerview.isVisible = false
+                isUpdateWeek = true
                 viewModel.updateWeek(WeekAction.ACTION_LAST_WEEK)
             })
         }
@@ -196,7 +172,7 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
     private fun setButtonNextWeekBehavior(binding: FragmentScheduleBinding, it: OffsetDateTime) {
         binding.buttonNextWeek.setColorFilter(resources.getColor(R.color.amazingtalker_green_900))
         binding.buttonNextWeek.setOnClickListener(View.OnClickListener {
-            binding.scheduleTimeRecyclerview.isVisible = false
+            isUpdateWeek = true
             viewModel.updateWeek(WeekAction.ACTION_NEXT_WEEK)
         })
     }
@@ -218,35 +194,6 @@ class ScheduleFragment : Fragment(), OnItemClickListener {
                 text = dateFormatter.format(element)
                 binding.tablayout.addTab(this)
             }
-        }
-    }
-
-    private fun updateAdapterList(
-        adapter: ScheduleTimeListAdapter,
-        teacherScheduleTimeList: List<IntervalScheduleTimeSlot>?
-    ) {
-        if (mCurrentTabTag.isEmpty()) return
-
-        if (getTeacherScheduleTimeListByFilterCurrentTabLocalTime(teacherScheduleTimeList) != null) {
-            getTeacherScheduleTimeListByFilterCurrentTabLocalTime(teacherScheduleTimeList)?.let {
-                adapter.addHeaderAndSubmitList(
-                    it
-                )
-            }
-            binding?.scheduleTimeRecyclerview?.scrollToPosition(0)
-        }
-    }
-
-    private fun getTeacherScheduleTimeListByFilterCurrentTabLocalTime(
-        teacherScheduleTimeList: List<IntervalScheduleTimeSlot>?
-    ): List<IntervalScheduleTimeSlot>? {
-        var currentTabLocalTime =
-            Instant.from(DateTimeFormatter.ISO_ZONED_DATE_TIME.parse(mCurrentTabTag))
-                .atOffset(ZoneOffset.UTC)
-                .getLocalOffsetDateTime()
-
-        return teacherScheduleTimeList?.filter { item ->
-            item.start.dayOfYear == currentTabLocalTime.dayOfYear
         }
     }
 
--- feature/teacherschedule/src/main/java/com/wei/amazingtalker_recruit/feature/teacherschedule/viewmodels/ScheduleViewModel.kt
+++ feature/teacherschedule/src/main/java/com/wei/amazingtalker_recruit/feature/teacherschedule/viewmodels/ScheduleViewModel.kt
@@ -3,21 +3,26 @@ package com.wei.amazingtalker_recruit.feature.teacherschedule.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wei.amazingtalker_recruit.core.data.repository.TeacherScheduleRepository
+import com.wei.amazingtalker_recruit.core.domain.GetTeacherScheduleTimeUseCase
 import com.wei.amazingtalker_recruit.core.extensions.getLocalOffsetDateTime
 import com.wei.amazingtalker_recruit.core.extensions.getUTCOffsetDateTime
 import com.wei.amazingtalker_recruit.core.model.data.IntervalScheduleTimeSlot
-import com.wei.amazingtalker_recruit.core.network.model.NetworkTeacherSchedule
+import com.wei.amazingtalker_recruit.core.model.data.ScheduleState
 import com.wei.amazingtalker_recruit.core.result.DataSourceResult
 import com.wei.amazingtalker_recruit.core.result.asDataSourceResult
+import com.wei.amazingtalker_recruit.feature.teacherschedule.utilities.TEACHER_SCHEDULE_TIME_INTERVAL
 import com.wei.amazingtalker_recruit.feature.teacherschedule.utilities.TEST_DATA_TEACHER_NAME
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.time.DayOfWeek
+import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
@@ -31,22 +36,17 @@ enum class WeekAction {
 
 @HiltViewModel
 class ScheduleViewModel @Inject constructor(
-    private val teacherScheduleRepository: TeacherScheduleRepository
+    private val teacherScheduleRepository: TeacherScheduleRepository,
+    private val getTeacherScheduleTimeUseCase: GetTeacherScheduleTimeUseCase
 ) : ViewModel() {
 
     private val _currentTeacherNameValue = MutableStateFlow("")
     val currentTeacherNameValue: StateFlow<String> get() = _currentTeacherNameValue
 
-    private val _currentSearchResult = MutableStateFlow<DataSourceResult<NetworkTeacherSchedule>>(DataSourceResult.Loading)
-    val currentSearchResult: StateFlow<DataSourceResult<NetworkTeacherSchedule>> get() = _currentSearchResult
-
-    private val _teacherScheduleTimeList = MutableStateFlow(emptyList<IntervalScheduleTimeSlot>())
-    val teacherScheduleTimeList: StateFlow<List<IntervalScheduleTimeSlot>>
-        get() = _teacherScheduleTimeList
+    private val _teacherScheduleTimeList =
+        MutableStateFlow<DataSourceResult<MutableList<IntervalScheduleTimeSlot>>>(DataSourceResult.Loading)
 
     private val _apiQueryStartedAtUTC = MutableStateFlow(OffsetDateTime.now())
-    val apiQueryStartedAtUTC: StateFlow<OffsetDateTime>
-        get() = _apiQueryStartedAtUTC
 
     private val _weekMondayLocalDate = MutableStateFlow(OffsetDateTime.now())
     val weekMondayLocalDate: StateFlow<OffsetDateTime>
@@ -64,6 +64,36 @@ class ScheduleViewModel @Inject constructor(
     val dateTabStringList: StateFlow<MutableList<OffsetDateTime>>
         get() = _dateTabStringList.asStateFlow()
 
+    private val _selectedTabTag = MutableStateFlow("")
+
+    val filteredTimeList: Flow<DataSourceResult<List<IntervalScheduleTimeSlot>>> =
+        combine(_teacherScheduleTimeList, _selectedTabTag) { result, tag ->
+            when (result) {
+                is DataSourceResult.Success -> {
+                    if (tag.isNotEmpty()) {
+                        var currentTabLocalTime =
+                            Instant.from(DateTimeFormatter.ISO_ZONED_DATE_TIME.parse(tag))
+                                .atOffset(ZoneOffset.UTC).getLocalOffsetDateTime()
+                        val filteredList = result.data.filter { item ->
+                            item.start.dayOfYear == currentTabLocalTime.dayOfYear
+                        }
+
+                        DataSourceResult.Success(filteredList)
+                    } else {
+                        DataSourceResult.Success(emptyList())
+                    }
+                }
+
+                is DataSourceResult.Error -> {
+                    DataSourceResult.Error(result.exception)
+                }
+
+                is DataSourceResult.Loading -> {
+                    DataSourceResult.Loading
+                }
+            }
+        }
+
     init {
         // Set initial values for the order
         resetWeekDate(OffsetDateTime.now(ZoneOffset.UTC))
@@ -73,6 +103,10 @@ class ScheduleViewModel @Inject constructor(
                 ChronoUnit.SECONDS
             ).toString()
         )
+    }
+
+    fun onTabSelected(tag: String) {
+        _selectedTabTag.value = tag
     }
 
     private fun resetWeekDate(apiQueryStartedAt: OffsetDateTime?) {
@@ -89,12 +123,11 @@ class ScheduleViewModel @Inject constructor(
 
         val weekStartFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val weekEndFormatter = DateTimeFormatter.ofPattern("MM-dd")
-        _weekLocalDateText.value =
-            "${weekStartFormatter.format(_weekMondayLocalDate.value)} - ${
-                weekEndFormatter.format(
-                    _weekSundayLocalDate.value
-                )
-            }"
+        _weekLocalDateText.value = "${weekStartFormatter.format(_weekMondayLocalDate.value)} - ${
+            weekEndFormatter.format(
+                _weekSundayLocalDate.value
+            )
+        }"
     }
 
     private fun setDateTabOptionsByLocalOffsetDateTime(offsetDateTime: OffsetDateTime) {
@@ -115,21 +148,46 @@ class ScheduleViewModel @Inject constructor(
         viewModelScope.launch {
             _currentTeacherNameValue.value = teacherName
 
-            _currentSearchResult.value = teacherScheduleRepository.getTeacherAvailability(teacherName, startedAtUTC)
-                .asDataSourceResult()
-                .stateIn(
+            teacherScheduleRepository.getTeacherAvailability(teacherName, startedAtUTC)
+                .asDataSourceResult().stateIn(
                     scope = viewModelScope,
                     started = SharingStarted.WhileSubscribed(5_000),
                     initialValue = DataSourceResult.Loading
-                ).collect{ result ->
-                    _currentSearchResult.value = result
+                ).collect { result ->
+                    when (result) {
+                        is DataSourceResult.Success -> {
+                            val scheduleTimeList = mutableListOf<IntervalScheduleTimeSlot>()
+
+                            scheduleTimeList.addAll(
+                                getTeacherScheduleTimeUseCase(
+                                    result.data.available,
+                                    TEACHER_SCHEDULE_TIME_INTERVAL,
+                                    ScheduleState.AVAILABLE
+                                )
+                            )
+                            scheduleTimeList.addAll(
+                                getTeacherScheduleTimeUseCase(
+                                    result.data.booked,
+                                    TEACHER_SCHEDULE_TIME_INTERVAL,
+                                    ScheduleState.BOOKED
+                                )
+                            )
+                            scheduleTimeList.sortedBy { scheduleTime -> scheduleTime.start }
+                            _teacherScheduleTimeList.value =
+                                DataSourceResult.Success(scheduleTimeList)
+                        }
+
+                        is DataSourceResult.Error -> {
+                            _teacherScheduleTimeList.value =
+                                DataSourceResult.Error(result.exception)
+                        }
+
+                        is DataSourceResult.Loading -> {
+                            _teacherScheduleTimeList.value = DataSourceResult.Loading
+                        }
+                    }
                 }
         }
-    }
-
-    fun setTeacherScheduleTimeList(teacherScheduleTimeList: List<IntervalScheduleTimeSlot>) {
-        _teacherScheduleTimeList.value =
-            teacherScheduleTimeList.sortedBy { scheduleTime -> scheduleTime.start }
     }
 
     fun updateWeek(action: WeekAction) {
