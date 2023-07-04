1a907891f071648057599dad738d34ce8c786b7d
Wei.He <wei_he@tk3c.com>
2023-07-04 16:37:39 +0800

    - Chore: 遷移 groovy to kts，project build.gradle。 - Chore: 遷移 groovy to kts，app module build.gradle。

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.navigation.safeargs.kotlin) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+}
