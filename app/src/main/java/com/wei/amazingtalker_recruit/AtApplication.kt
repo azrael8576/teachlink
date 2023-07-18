ab9c84ea9ef48b9a770ed5fb281f518ad2941ad8
Wei.He <wei_he@tk3c.com>
2023-07-18 13:56:00 +0800

    - Feat: 新增 core:data interface TeacherScheduleRepository，以利測試方便替換 FakeRepository。

--- app/src/main/java/com/wei/amazingtalker_recruit/AtApplication.kt
+++ app/src/main/java/com/wei/amazingtalker_recruit/AtApplication.kt
@@ -1,6 +1,7 @@
 package com.wei.amazingtalker_recruit
 
 import android.app.Application
+import com.wei.amazingtalker_recruit.core.common.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
--- core/data/src/main/java/com/wei/amazingtalker_recruit/core/data/di/DataModule.kt
+++ core/data/src/main/java/com/wei/amazingtalker_recruit/core/data/di/DataModule.kt
@@ -1,5 +1,7 @@
 package com.wei.amazingtalker_recruit.core.data.di
 
+import com.wei.amazingtalker_recruit.core.data.repository.DefaultTeacherScheduleRepository
+import com.wei.amazingtalker_recruit.core.data.repository.TeacherScheduleRepository
 import com.wei.amazingtalker_recruit.core.data.utils.ConnectivityManagerNetworkMonitor
 import com.wei.amazingtalker_recruit.core.data.utils.NetworkMonitor
 import dagger.Binds
@@ -10,6 +12,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 interface DataModule {
+
+    @Binds
+    fun bindsTeacherScheduleRepository(
+        teacherScheduleRepository: DefaultTeacherScheduleRepository,
+    ): TeacherScheduleRepository
 
     @Binds
     fun bindsNetworkMonitor(
