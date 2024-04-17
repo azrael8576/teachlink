64c8a41015c2b65bbe627fee21cf51b9a0113de7
WeiHe <azrael8576love2008@gmail.com>
2024-04-18 00:00:05 +0800

    Release.v.0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
          *
          * XYZ: App version (050 for 0.5.0)
          */
-        versionCode = 3414070
+        versionCode = 3414071
         /**
          * SemVer major.minor.patch
          */
-        versionName = "0.7.0"
+        versionName = "0.7.1"
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.wei.teachlink.core.testing.TlTestRunner"
